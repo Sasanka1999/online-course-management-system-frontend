35d096ebbfe7fa05bf06ef66c1a9d20a8c38a3bf
Ssanka1999 <sasankapabasara3@gmail.com>
2024-12-06 23:47:55 +0530

    create main home  dashboard

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+function Dashboard() {
+  return (
+    <Box>
+      <Typography variant="h4">Dashboard</Typography>
+      <Typography>Welcome to the Dashboard!</Typography>
+    </Box>
+  );
+}
+
+export default Dashboard;
